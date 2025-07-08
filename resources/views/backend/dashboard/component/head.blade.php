24829a6c05ca935ee8cfe5c08255b920463298d9
thaothanh <thaothanh19072k1@gmail.com>
2025-07-08 18:29:48 +0700

    feat: dashboard page

---
+++ resources/views/backend/dashboard/component/head.blade.php
@@ -0,0 +1,11 @@
+{{-- <base href=" {{ env('APP_URL')}} "> --}}
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>INSPINIA | Dashboard v.2</title>
+
+<link href="{{ asset('backend/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('backend/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+
+<link href="{{ asset('backend/css/animate.css') }}" rel="stylesheet">
+<link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
