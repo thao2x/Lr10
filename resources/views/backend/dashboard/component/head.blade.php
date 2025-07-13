670d1a46e43dc3cefbff769a070e44d179812582
thaothanh <thaothanh19072k1@gmail.com>
2025-07-13 10:13:51 +0700

    fix

--- resources/views/backend/dashboard/component/head.blade.php
+++ resources/views/backend/dashboard/component/head.blade.php
@@ -14,9 +14,5 @@
     @foreach ($config['css'] as $val)
         <link href="{{ asset($val) }}" rel="stylesheet"></link>
     @endforeach
-<<<<<<< HEAD
 @endif
 <script src="{{ asset('backend/js/jquery-3.1.1.min.js') }}"></script>
-=======
-@endif
->>>>>>> 91037d29506f6dced6d6a5c73a5695397a43f39b
--- resources/views/backend/dashboard/component/script.blade.php
+++ resources/views/backend/dashboard/component/script.blade.php
@@ -1,8 +1,4 @@
 <!-- Mainly scripts -->
-<<<<<<< HEAD
-=======
-<script src="{{ asset('backend/js/jquery-3.1.1.min.js') }}"></script>
->>>>>>> 91037d29506f6dced6d6a5c73a5695397a43f39b
 <script src="{{ asset('backend/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('backend/js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
 <script src="{{ asset('backend/js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
